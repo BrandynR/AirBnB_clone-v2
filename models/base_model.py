8e21e2d533385ec521f6d0f8cc7cd92519f8d1f7
Jp203 <jp203@icloud.com>
2019-12-20 15:03:50 -0500

    Task 6

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,29 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import String, Column, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+"""This is the base for any models that extend this class"""
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column(String(60),
+                unique=True,
+                nullable=False,
+                primary_key=True)
+
+    created_at = Column(DateTime,
+                        default=datetime.utcnow(),
+                        nullable=False)
+
+    updated_at = Column(DateTime,
+                        default=datetime.utcnow(),
+                        nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -20,16 +37,16 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,7 +65,13 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
+
+    def delete(self):
+        """delete the current instance from storage (models.storage)
+        by calling the method delete"""
+        models.storage.delete(self)
 
     def to_dict(self):
         """creates dictionary of the class  and returns
@@ -56,6 +79,8 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()

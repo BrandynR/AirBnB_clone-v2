46965f689d4228570066cbe0a85b66818aeeb3fb
BrandynR <BrandynR@github.com>
2019-12-20 21:03:19 +0000

    Updated state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
@@ -8,4 +9,16 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        """get the cities with state_id equal"""
+        rtrn = []
+        for key, value in storage.all().keys():
+            if key.split(".")[0] == "City":
+                if value.state_id == self.id:
+                    rtrn.append(value)
+        return rtrn

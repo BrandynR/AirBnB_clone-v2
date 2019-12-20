a072388a9cbe79caa09a4e24f130c5a9c4737570
BrandynR <BrandynR@github.com>
2019-12-20 21:49:08 +0000

    Updates class place

--- models/place.py
+++ models/place.py
@@ -18,14 +18,56 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+__tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"),nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", "place")
+
+    amenities = relationship(
+        "Amenity", backref="place_amenities",
+        secondary="place_amenity",
+        viewonly=False)
+
+    @property
+    def reviews(self):
+        """ getter attribute for reviews of places """
+        obj = storage.all()
+        reviews = []
+        print("OBJ:", obj)
+        print("MY DICT:", my_dict)
+        for key, value in my_dict.items():
+            if "Review" in key and value.place_id == self.id:
+                reviews.append(value)
+        return reviews
+
+    @property
+    def amenities(self):
+        """ getter attribute for amenities of places """
+        obj = storage.all()
+        amenities = []
+        for key, value in obj:
+            if "Amenity" in key and value.place_id == self.id:
+                amenities.append(value)
+        return amenities
+
+    place_amenity = Table(
+                            "place_amenity", Base.metadata,
+                            Column('place_id',
+                                   String(60),
+                                   ForeignKey('places.id'),
+                                   primary_key=True,
+                                   nullable=False),
+                            Column('amenity_id',
+                                   String(60),
+                                   ForeignKey('amenities.id'),
+                                   primary_key=True,
+                                   nullable=False))

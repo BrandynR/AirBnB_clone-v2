971dbbdfc4513e52fd6331fb302589e399324417
BrandynR <BrandynR@github.com>
2019-12-20 21:02:49 +0000

    Added def delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,17 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls:
+            objs_list = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    objs_list[key] = value
+            return objs_list
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +61,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
